a9220d5199a0a3477e81768814fe08919b109268
romannikiforov <escaro@ukr.net>
2020-02-19 11:52:19 +0200

    start 04_error_boundary

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Planets from "./components/Planets";
+
+function App() {
+  return (
+    <div className="container">
+      <Planets />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Planets.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Planets extends Component {
+  render() {
+    return (
+      <ul>
+        <li>Planets - 5</li>
+      </ul>
+    );
+  }
+}
+
+export default Planets;
