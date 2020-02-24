ca19f79c6be4931749c39bfa4d2ecc03cc0c2fda
romannikiforov <escaro@ukr.net>
2020-02-24 11:53:22 +0200

    start 06_compound_components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import Tabs from "./components/Tabs/Tabs";
+import LoginForm from "./components/Forms/LoginForm";
+import SignupForm from "./components/Forms/SignupForm";
+import "./App.css";
 
+const tabData = [
+  {
+    label: "Login",
+    content: <LoginForm />
+  },
+  {
+    label: "Signup",
+    content: <SignupForm />
+  }
+];
 
 function App() {
   return (
     <div className="container">
-        <h1>Hello React</h1>
+      <div className="App">
+        <Tabs data={tabData} />
+      </div>
     </div>
   );
 }
+
 export default App;
