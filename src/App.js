37ad0484d72a1021e2cb03db35adf137a9c3ec58
roman <escaro@ukr.net>
2020-06-22 12:46:22 +0300

    fixed component App - refactor to class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
 import Planets from "./components/Planets";
 
-function App() {
-  return (
-    <div className="container">
-      <Planets />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Planets />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -3,16 +3,27 @@ import React, { Component } from "react";
 export class Planets extends Component {
   state = {
     planets: [],
+    loading: false,
   };
 
   async componentDidMount() {
-    const response = await fetch("https://swapi.dev/api/planets");
-    const data = await response.json();
-    this.setState({ planets: data.results });
+    this.setState({ loading: true });
+    try {
+      const response = await fetch("https://swapi.dev/api/planets");
+      const data = await response.json();
+      this.setState({ planets: data.results });
+    } catch (err) {
+      console.log(err);
+    } finally {
+      this.setState({ loading: false });
+    }
   }
 
   render() {
-    const { planets } = this.state;
+    const { planets, loading } = this.state;
+    if (loading) {
+      return <h1>Loading ...</h1>;
+    }
     return (
       <ul>
         {planets.map((planet) => (
