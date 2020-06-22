301d5c50269c62f3bc2cf4000138cae3d5f8b450
roman <escaro@ukr.net>
2020-06-22 12:33:48 +0300

    fixed component Planets

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
 
 export class Planets extends Component {
+  state = {
+    planets: [],
+  };
+
+  async componentDidMount() {
+    const response = await fetch("https://swapi.dev/api/planets");
+    const data = await response.json();
+    this.setState({ planets: data.results });
+  }
+
   render() {
+    const { planets } = this.state;
     return (
       <ul>
-        <li>Planets - 5</li>
+        {planets.map((planet) => (
+          <li key={planet.name}>
+            {planet.name} - {planet.films.length}
+          </li>
+        ))}
       </ul>
     );
   }
